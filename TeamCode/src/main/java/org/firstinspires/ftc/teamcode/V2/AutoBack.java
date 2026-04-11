8fe652939b89e1f166c3c8b1b012f1f22c4ce735
Antony Rios Sanchez <152814474+PixelisRetr0@users.noreply.github.com>
2026-04-11 12:57:37 -0500

    Blue Back Auto Working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/AutoBack.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/AutoBack.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.V2;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.Path;
@@ -12,19 +13,20 @@ import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
 import org.firstinspires.ftc.teamcode.V2.Libs.Commands;
 
 
-public abstract class AutoBack extends AutoBase{
+public abstract class AutoBack extends AutoBase {
     @Autonomous(group = "Red")
-    public static class AutoRedBack extends AutoBack{
+    public static class AutoRedBack extends AutoBack {
         @Override
         public void initialize() {
             super.initialize();
             setRedAlliance();
         }
     }
+
     @Autonomous(group = "Blue")
-    public static class AutoBlueBack extends AutoBack{
+    public static class AutoBlueBack extends AutoBack {
         @Override
-        public void initialize(){
+        public void initialize() {
             super.initialize();
             setBlueAlliance();
         }
@@ -33,33 +35,41 @@ public abstract class AutoBack extends AutoBase{
     @Override
     protected void configureCommands() {
         Follower follower = drive.getFollower();
-
+        PathChain drivetoShoot;
+        PathChain driveAway;
         Pose startpose = new Pose(57.146, 8.196, Math.toRadians(90));
-        PathChain drivetoShoot = follower.pathBuilder()
+        drivetoShoot = follower.pathBuilder()
                 .addPath(
-                        new BezierLine(
+                        new BezierCurve(
                                 startpose,
-                                new Pose(62.482, 13.437)
+                                new Pose(57.806, 18.558),
+                                new Pose(66.065, 9.630)
                         )
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(120))
                 .build();
 
-       PathChain driveAway = follower.pathBuilder()
+        driveAway = follower.pathBuilder()
                 .addPath(
-                        new BezierLine(
-                                new Pose(62.482, 13.437),
-                                new Pose(58.981, 42.589)
+                        new BezierCurve(
+                                new Pose(66.065, 9.630),
+                                new Pose(61.844, 36.083),
+                                new Pose(41.513, 36.767)
                         )
                 )
                 .setTangentHeadingInterpolation()
                 .build();
+        drive.getFollower().
 
-    drive.getFollower().setStartingPose(startpose);
-    schedule(Commands.sequence(
-        drive.follow(drivetoShoot),
-        visionShoot().withTimeout(10*1000),
-        drive.follow(driveAway)
-    ));
+                setStartingPose(startpose);
+
+        schedule(Commands.sequence(
+                drive.follow(drivetoShoot),
+
+                visionShoot().
+
+                        withTimeout(10 * 1000),
+                drive.follow(driveAway)
+        ));
     }
 }
